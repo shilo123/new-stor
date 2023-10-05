b490c54d83d48882bdebc5d41823352869883035
shilo <haz.shilo@gmail.com>
2023-10-05 17:07:19 +0300

    Fix transpileDependencies config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 // const { defineConfig } = require("@vue/cli-service");
+// module.exports = {
+//   transpileDependencies: true,
+// };
+//
 module.exports = {
-  transpileDependencies: true,
+  transpileDependencies: [],
 };
